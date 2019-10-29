972acd2db228cfbe7cd472bd930d507375ee16f8
Nicholas Upshaw <nicholas.upsha@yahoo.com>
2019-10-29 18:29:29 -0400

    Start of liribot

---
+++ liribot.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+
+// concert this
+function name() {
+    console.log("concert-this")
+}
+
+// Spotify this - song
+function name() {
+    console.log("Spotify-this")
+}
+
+// movie this
+function name() {
+    console.log("Movie-this")
+}
+// do what it says
+function name() {
+    console.log("Do what it says")
+}
