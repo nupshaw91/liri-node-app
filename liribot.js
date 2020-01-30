33259e8ca1ed56366ad8a95886c849a260640d28
Nicholas Upshaw <nicholas.upsha@yahoo.com>
2020-01-30 13:19:36 -0500

    Adding node packages and requiring thing for the woking out

--- liribot.js
+++ liribot.js
@@ -1,21 +1,33 @@
 require("dotenv").config();
 var keys = require("./keys.js");
+var spotify = require("node-spotify-api");
+var axios = require("axios");
+var fs = require("fs");
+var moment = require("moment")
 
+var argOne = process.argv[2];
+var argTwo = process.argv.slice(3).join(" ")
 // concert this
-function name() {
+function liriRun(argOne, argTwo) {
     console.log("concert-this")
-}
 
-// Spotify this - song
-function name() {
-    console.log("Spotify-this")
-}
 
-// movie this
-function name() {
-    console.log("Movie-this")
-}
-// do what it says
-function name() {
-    console.log("Do what it says")
+    // Spotify this - song
+    spotify
+        .search({ type: 'track', query: 'All the Small Things' })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
+
+    // movie this
+    function name() {
+        console.log("Movie-this")
+    }
+    // do what it says
+    function name() {
+        console.log("Do what it says")
+    }
 }
